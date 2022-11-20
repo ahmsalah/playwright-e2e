cb90560aedefbbec94eb4d77420e63eff75f32bd
ahmsalah <aemdsalah@gmail.com>
2022-11-21 00:48:01 +0200

    chore: add env variables

--- tests/authentication/utils.ts
+++ tests/authentication/utils.ts
@@ -1,8 +1,16 @@
 import { Page } from "@playwright/test";
 
+const {
+  WEB_APP_URL = "",
+  USER_1_EMAIL = "",
+  USER_1_PASS = "",
+  USER_2_EMAIL = "",
+  USER_2_PASS = "",
+} = process.env;
+
 export const users = [
-  { username: "salah+e2e-1@agreewe.com", password: "12345678" },
-  { username: "salah+e2e-2@agreewe.com", password: "12345678" },
+  { username: USER_1_EMAIL, password: USER_1_PASS },
+  { username: USER_2_EMAIL, password: USER_2_PASS },
 ];
 
 type ILoginParams = {
@@ -12,7 +20,7 @@ type ILoginParams = {
 };
 
 export async function login({ page, username, password }: ILoginParams) {
-  await page.goto("https://mobile.agreewe.com/");
+  await page.goto(WEB_APP_URL);
   await page.getByRole("button", { name: "Log In" }).click();
   await page.getByTestId("email").fill(username);
   await page.getByTestId("password").fill(password);
